312f57e9a0301708a8c4c15db2022f18f5334ab6
Toda Racing <kennynguyen522@gmail.com>
2021-12-30 16:34:24 -0800

    Pushing to Heroku

---
+++ client/webpack.config.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: "development",
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+    },
+    devServer: {
+      hot: true,
+      static: "./dist",
+      open: false,
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "jate",
+        template: "./index.html",
+      }),
+      new InjectManifest({
+        swSrc: "./src/src-sw.js",
+        swDest: "src-sw.js",
+        exclude: [/\.map$/, /asset-manifest\.json$/],
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another text editor",
+        short_name: "jate",
+        description: "A simple pwa text editor",
+        background_color: "#ffffff",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        crossorigin: "use-credentials",
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // different sizes
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
